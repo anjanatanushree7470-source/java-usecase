c87943bbb7656c74d13126f44c4894be0c8efad7
anjanatanushree7470-source <anjanatanushree7470@gmail.com>
2026-01-29 16:43:51 +0530

    Add files via upload

---
+++ ListExample.java
@@ -0,0 +1,17 @@
+package Collection;
+
+import java.util.*;
+
+public class ListExample {
+public static void main(String[] args)
+	{
+List<String> names=new ArrayList<>();
+//ArrayList<String> name= new ArrayList<>();
+//ArrayList<String> name1= new ArrayList();
+		names.add("Renu ");
+		names.add("Sai");
+		names.add("Renu");
+		System.out.println(names);
+	}
+ 
+}
